e94f89ab3035355d3577958ffe85844a80b2ca98
Maciej Seta <8685951+setamaciej@users.noreply.github.com>
2020-07-21 13:47:54 +0200

    Add files via upload

---
+++ game/Room.java
@@ -0,0 +1,152 @@
+package game;
+
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+
+public class Room {
+    private int width;
+    private int height;
+    private Snake snake;
+    private Mouse mouse;
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+        game = this;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+
+
+    public void run() {
+
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
+
+
+        while (snake.isAlive()) {
+
+            if (keyboardObserver.hasKeyEvents()) {
+                KeyEvent event = keyboardObserver.getEventFromTop();
+
+                if (event.getKeyChar() == 'q') return;
+
+                // If "left arrow", then move the figure to the left
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    snake.setDirection(SnakeDirection.LEFT);
+                    // If "right arrow", then move the figure to the right
+                else if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+                    snake.setDirection(SnakeDirection.RIGHT);
+                    // If "up arrow", then move the figure up
+                else if (event.getKeyCode() == KeyEvent.VK_UP)
+                    snake.setDirection(SnakeDirection.UP);
+                    // If "down arrow", then move the figure down
+                else if (event.getKeyCode() == KeyEvent.VK_DOWN)
+                    snake.setDirection(SnakeDirection.DOWN);
+            }
+
+            snake.move();
+            print();
+            sleep();
+        }
+
+
+        System.out.println("Game Over!");
+    }
+
+
+    public void print() {
+
+        int[][] matrix = new int[height][width];
+
+
+        ArrayList<SnakeSection> sections = new ArrayList<SnakeSection>(snake.getSections());
+        for (SnakeSection snakeSection : sections) {
+            matrix[snakeSection.getY()][snakeSection.getX()] = 1;
+        }
+
+        matrix[snake.getY()][snake.getX()] = snake.isAlive() ? 2 : 4;
+
+        matrix[mouse.getY()][mouse.getX()] = 3;
+
+        String[] symbols = {" . ", " x ", " X ", "^_^", "RIP"};
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(symbols[matrix[y][x]]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+
+    public void eatMouse() {
+        createMouse();
+    }
+
+
+    public void createMouse() {
+        int x = (int) (Math.random() * width);
+        int y = (int) (Math.random() * height);
+
+        mouse = new Mouse(x, y);
+    }
+
+
+    public static Room game;
+
+    public static void main(String[] args) {
+        game = new Room(20, 20, new Snake(10, 10));
+        game.snake.setDirection(SnakeDirection.DOWN);
+        game.createMouse();
+        game.run();
+    }
+
+    private int initialDelay = 520;
+    private int delayStep = 20;
+
+
+    public void sleep() {
+        try {
+            int level = snake.getSections().size();
+            int delay = level < 15 ? (initialDelay - delayStep * level) : 200;
+            Thread.sleep(delay);
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
---
+++ game/SnakeDirection.java
@@ -0,0 +1,9 @@
+package game;
+
+
+public enum SnakeDirection {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+}
